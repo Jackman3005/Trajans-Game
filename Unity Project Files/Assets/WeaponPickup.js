b4e3de286babcab4ac223868962130b2c1865ca7
Tyler <kairu43@yahoo.com>
2012-11-23 22:51:07 -0500

    Tried to prevent showing two weapon messages at the same time, but in short, just keep the colliders far enough apart.
    Booleans didn't work, string checks didn't work.
    
    This is minor, just a few modifications to colliders and code clean-up. Next commit will be trying to move hud stuff into Jack's HUD script

--- Unity Project Files/Assets/WeaponPickup.js
+++ Unity Project Files/Assets/WeaponPickup.js
@@ -12,10 +12,17 @@ var MedievalBlade:Texture2D;
 var Weapon2:Texture2D;
 var Weapon3:Texture2D;
 
+//once equipped, can't un-equip- MIGHT CHANGE THIS
+var leftEquipped:boolean = false;
+var rightEquipped:boolean = false;
+
+var weapon=["Medieval Blade","Weapon2","Weapon3"];
+var weaponNum:int;//stores array value
+
 function Start () {
 	medievalBlade = GameObject.FindGameObjectWithTag("MedievalBlade");
-	weapon2  = GameObject.FindGameObjectWithTag("Weapon2");
-	weapon3  = GameObject.FindGameObjectWithTag("Weapon3");
+	weapon2       = GameObject.FindGameObjectWithTag("Weapon2");
+	weapon3       = GameObject.FindGameObjectWithTag("Weapon3");
 }
 
 function Update () {
@@ -38,48 +45,66 @@ function Update () {
 		}
 	}*/
 	if(selectedWeapon == "MedievalBlade")
-	{		
-		if(Input.GetKeyDown(KeyCode.Keypad1)||Input.GetKeyDown(KeyCode.Alpha1))
+	{	
+		weaponNum=0;
+		
+		if((Input.GetKeyDown(KeyCode.Keypad1)||Input.GetKeyDown(KeyCode.Alpha1))
+		&& !leftEquipped)
 		{
 			Destroy(medievalBlade);
 			InGameGUI.toDrawLeft = selectedWeapon;
 			selectedWeapon = "";
+			leftEquipped = true;
 		}
-		else if(Input.GetKeyDown(KeyCode.Keypad5)||Input.GetKeyDown(KeyCode.Alpha5))
+		else if((Input.GetKeyDown(KeyCode.Keypad5)||Input.GetKeyDown(KeyCode.Alpha5))
+		&& !rightEquipped)
 		{
 			Destroy(medievalBlade);
 			InGameGUI.toDrawRight = selectedWeapon;
 			selectedWeapon = "";
+			rightEquipped = true;
 		}
 	}
 	else if(selectedWeapon == "Weapon2")
-	{
-		if(Input.GetKeyDown(KeyCode.Keypad1)||Input.GetKeyDown(KeyCode.Alpha1))
+	{	
+		weaponNum=1;
+		
+		if((Input.GetKeyDown(KeyCode.Keypad1)||Input.GetKeyDown(KeyCode.Alpha1))
+		&& !leftEquipped)
 		{
 			Destroy(weapon2);
 			InGameGUI.toDrawLeft = selectedWeapon;
 			selectedWeapon = "";
+			leftEquipped = true;
 		}
-		else if(Input.GetKeyDown(KeyCode.Keypad5)||Input.GetKeyDown(KeyCode.Alpha5))
+		else if((Input.GetKeyDown(KeyCode.Keypad5)||Input.GetKeyDown(KeyCode.Alpha5))
+		&& !rightEquipped)
 		{
 			Destroy(weapon2);
 			InGameGUI.toDrawRight = selectedWeapon;
 			selectedWeapon = "";
+			rightEquipped = true;
 		}
 	}
 	else if(selectedWeapon == "Weapon3")
 	{
-		if(Input.GetKeyDown(KeyCode.Keypad1)||Input.GetKeyDown(KeyCode.Alpha1))
+		weaponNum=3;
+		
+		if((Input.GetKeyDown(KeyCode.Keypad1)||Input.GetKeyDown(KeyCode.Alpha1))
+		&& !leftEquipped)
 		{
 			Destroy(weapon3);
 			InGameGUI.toDrawLeft = selectedWeapon;
 			selectedWeapon = "";
+			leftEquipped = true;
 		}
-		else if(Input.GetKeyDown(KeyCode.Keypad5)||Input.GetKeyDown(KeyCode.Alpha5))
+		else if((Input.GetKeyDown(KeyCode.Keypad5)||Input.GetKeyDown(KeyCode.Alpha5))
+		&& !rightEquipped)
 		{
 			Destroy(weapon3);
 			InGameGUI.toDrawRight = selectedWeapon;
 			selectedWeapon = "";
+			rightEquipped = true;
 		}
 	}
 }
@@ -92,21 +117,27 @@ function OnTriggerEnter(col:Collider)
 	}
 }
 
+/*function OnTriggerExit(other:Collider){
+	if(other.gameObject.tag=="Player")
+		selectedWeapon ="";
+}*/
+
 function OnGUI(){
 	if(selectedWeapon == "MedievalBlade")
 	{
 		GUI.Label(Rect((Screen.width/2)-50,Screen.height-500,(Screen.width/2)-250,Screen.height-150),
-		"         Equip Sword?\n\nPress 1 for LeftHand Equip\nPress 5 for RightHand Equip");
+		"         Equip " + weapon[weaponNum] +"?\n\nPress 1 for LeftHand Equip\nPress 5 for RightHand Equip");
 	}
 	else if(selectedWeapon == "Weapon2")
 	{
 		GUI.Label(Rect((Screen.width/2)-50,Screen.height-500,(Screen.width/2)-250,Screen.height-150),
-		"         Equip Weapon2?\n\nPress 1 for LeftHand Equip\nPress 5 for RightHand Equip");
+		"         Equip " + weapon[weaponNum] +"?\n\nPress 1 for LeftHand Equip\nPress 5 for RightHand Equip");
 	}
 	else if(selectedWeapon == "Weapon3")
 	{
 		GUI.Label(Rect((Screen.width/2)-50,Screen.height-500,(Screen.width/2)-250,Screen.height-150),
-		"         Equip Weapon3?\n\nPress 1 for LeftHand Equip\nPress 5 for RightHand Equip");
+		"         Equip " + weapon[weaponNum] +"?\n\nPress 1 for LeftHand Equip\nPress 5 for RightHand Equip");
 	}
 	else{/*no weapon selected*/}
+
 }
