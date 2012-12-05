7925dcd607ea514700ff961614d54ebda38fd1b8
Jack <Jack@Coy>
2012-12-05 17:42:15 -0500

    Updated EnemyUI script to only follow player at certain times.  The enemy now has a field of view in which if it can see you it will attack you if you are within the specified range
    Also there is a shorter range in which if you walk up behind the enemy they will turn around and attack you even if they cannot see you.

--- Unity Project Files/Assets/Standard Assets/Scripts/EnemyAI.js
+++ Unity Project Files/Assets/Standard Assets/Scripts/EnemyAI.js
@@ -4,12 +4,19 @@ public var movementSpeed : double = 1;
 public var attackSpeed : double = 1;
 public var attackDamage : double = 5;
 public var attackRange : double = 2;
+public var fieldOfViewInDegrees : float = 60;
+public var maxDistEnemyCanSeePlayer : float = 60;
+public var distThatEnemyWillAttackPlayerWithoutSeeingThem : float = 5;
 
 private var playerIsAttackable : boolean = false;
 private var player : GameObject;
 private var timeSinceLastAttack : double;
 private var controller : CharacterController;
 private var rotationDamping : double;
+private var directionOfPlayer : Vector3;
+
+enum EnemyState {patrol,attack};
+private var currState : EnemyState = EnemyState.patrol;
 
 function Awake () {
 	controller = GetComponent (CharacterController);
@@ -24,43 +31,48 @@ function tryToAttackPlayer(){
 }
 
 
-
 function Start () {
 	player = GameObject.FindGameObjectWithTag("Player");
 	timeSinceLastAttack = attackSpeed;
 }
 
 function Update () {
-	var playerX : double = player.transform.position.x;
-	var playerY : double = player.transform.position.y;
-	var playerZ : double = player.transform.position.z;
-	var direction : Vector3 = new Vector3(playerX - transform.position.x,playerY - transform.position.y, playerZ - transform.position.z);
-	
-//	 var rotation : Quaternion = new Quaternion();
-//   	rotation.SetLookRotation(direction);
-//	transform.rotation.y = rotation.y;
-
-	var rotation = Quaternion.LookRotation(direction);
-	transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * rotationDamping);
-
-
-	playerIsAttackable = direction.magnitude <= attackRange;
+	directionOfPlayer = player.transform.position - transform.position;	
+	playerIsAttackable = directionOfPlayer.magnitude <= attackRange;
 	tryToAttackPlayer();
-
-
-	
-//	if (!playerIsAttackable){
-//		transform.position.x += direction.normalized.x * movementSpeed*Time.deltaTime;
-//		transform.position.z += direction.normalized.z * movementSpeed*Time.deltaTime;
-//	}
-
-
-	//The pushDownOffset is used to make sure the enemy goes off of cliffs appropriately. Gravity by itself does not make it fall fast enough.
-	var pushDownOffset : float = Mathf.Max(controller.stepOffset, Vector3(direction.x, 0, direction.z).magnitude);
-	direction -= pushDownOffset * Vector3.up;
-	
-	controller.Move(direction.normalized*movementSpeed*Time.deltaTime);
-	
+	if (PlayerIsInRangeAndSight()){
+		FollowAndLookAtPlayer();
+	}
 	
 }
 
+function PlayerIsInRangeAndSight(){
+	var forwardFacingAngle : float = transform.rotation.y;
+	var angleTowardsPlayer : float = Vector3.Angle(directionOfPlayer, transform.forward);
+	
+	var returnValue : boolean = ((forwardFacingAngle - (fieldOfViewInDegrees/2)) < angleTowardsPlayer) && (angleTowardsPlayer < (forwardFacingAngle + (fieldOfViewInDegrees/2)));
+	Debug.Log("facing: " + forwardFacingAngle);
+	Debug.Log(((forwardFacingAngle - (fieldOfViewInDegrees/2)) < angleTowardsPlayer) + "  " + ((forwardFacingAngle + (fieldOfViewInDegrees/2)) > angleTowardsPlayer));
+	Debug.Log("Player: " +angleTowardsPlayer);
+	returnValue = (returnValue && directionOfPlayer.magnitude < maxDistEnemyCanSeePlayer) || (directionOfPlayer.magnitude < distThatEnemyWillAttackPlayerWithoutSeeingThem);
+	return returnValue;
+}
+
+function FollowAndLookAtPlayer(){
+	var point : Vector3 = player.transform.position;
+	point.y = transform.position.y;
+	transform.LookAt(point);
+
+	if (!playerIsAttackable){
+		//The pushDownOffset is used to make sure the enemy goes off of cliffs appropriately. Gravity by itself does not make it fall fast enough.
+		var pushDownOffset : float = Mathf.Max(controller.stepOffset, Vector3(directionOfPlayer.x, 0, directionOfPlayer.z).magnitude);
+		directionOfPlayer -= pushDownOffset * Vector3.up;
+		controller.Move(directionOfPlayer.normalized*movementSpeed*Time.deltaTime);
+	}
+	
+  	 var hit : RaycastHit;
+  	 var rayStartPos : Transform = transform;
+    if (Physics.Raycast (transform.position, Vector3.forward, hit)) {
+        Debug.Log(hit.collider.gameObject.tag);
+    }
+}
