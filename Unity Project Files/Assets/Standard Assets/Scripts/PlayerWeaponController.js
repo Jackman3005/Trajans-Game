0f13e9735cff3f64868e66a223bac1b690a3b6b0
Jackman3005 <Jackman3000@gmail.com>
2012-12-08 15:49:31 -0500

    Added WeaponScript.   Updated PlayerWeaponController

--- Unity Project Files/Assets/Standard Assets/Scripts/PlayerWeaponController.js
+++ Unity Project Files/Assets/Standard Assets/Scripts/PlayerWeaponController.js
@@ -1,7 +1,9 @@
 #pragma strict
 
-private var currentLeftWeapon : GameObject;
-private var currentRightWeapon : GameObject;
+private var currentMainHandWeapon : GameObject = null;
+private var currentOffHandWeapon : GameObject = null;
+private var currentMainHandWeaponScript : WeaponScript = null;
+private var currentOffHandWeaponScript : WeaponScript = null;
 private var isHoldingTwoHandedWeapon : boolean = false;
 
 function tryToAttackWithLeftWeapon(enemyAIScript : EnemyAI, distanceToEnemy : double){
@@ -12,5 +14,60 @@ function tryToAttackWithLeftWeapon(enemyAIScript : EnemyAI, distanceToEnemy : do
 
 function tryToAttackWithRightWeapon(enemyAIScript : EnemyAI, distanceToEnemy : double){
 		enemyAIScript.addHealth(-1);
-	
+}
+
+function assignMainHandWeapon (weapon : GameObject){
+	if (currentMainHandWeapon == null){
+		currentMainHandWeapon = weapon;
+		currentMainHandWeaponScript = weapon.GetComponent(WeaponScript);
+		isHoldingTwoHandedWeapon = currentMainHandWeaponScript.isTwoHanded;
+	}
+}
+
+function assignOffHandWeapon (weapon : GameObject){
+	if (currentOffHandWeapon == null && !isHoldingTwoHandedWeapon){
+		currentOffHandWeaponScript = weapon.GetComponent(WeaponScript);
+		if (currentOffHandWeaponScript.isTwoHanded && currentMainHandWeapon == null){
+			//If the weapon to be picked up is two-handed and we don't already have a mainhand weapon
+			//Then instead equip the two hander to the main hand.
+			currentMainHandWeapon = weapon;
+			currentMainHandWeaponScript = currentOffHandWeaponScript;
+			currentOffHandWeaponScript = null;
+			isHoldingTwoHandedWeapon = true;
+		}
+		else {
+			//If the weapon is not a two-handed weapon then equip it.
+			currentOffHandWeapon = weapon;
+		}
+	}
+}
+
+function dropMainHandWeapon (){
+	currentMainHandWeapon = null;
+	currentMainHandWeaponScript = null;
+	if (isHoldingTwoHandedWeapon){
+		isHoldingTwoHandedWeapon = false;
+	}
+}
+
+function dropOffHandWeapon (){
+	currentOffHandWeapon = null;
+	currentOffHandWeaponScript = null;
+}
+
+function getMainHandWeaponTexture(){
+	if (currentMainHandWeaponScript != null){
+		return currentMainHandWeaponScript.weaponImage;
+	}
+	return null;
+}
+
+function getOffHandWeaponTexture(){
+	if (isHoldingTwoHandedWeapon){
+		return currentMainHandWeaponScript.weaponImage;
+	}
+	if (currentOffHandWeaponScript != null){
+		return currentOffHandWeaponScript.weaponImage;
+	}
+	return null;
 }
---
+++ Unity Project Files/Assets/Standard Assets/Scripts/WeaponScript.js
@@ -0,0 +1,7 @@
+#pragma strict
+
+public var attacksPerSecond : double = 1;
+public var attackDamage : double = 1;
+public var attackRange : double = 5;
+public var weaponImage : Texture2D;
+public var isTwoHanded : boolean = false;
