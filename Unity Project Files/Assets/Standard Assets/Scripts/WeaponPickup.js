07272d9d67f985038a880dde3c1005f088795fc0
Tyler Cooley <kairu43@yahoo.com>
2012-12-08 11:43:25 -0500

    Cleaned up WeaponPickup.js, and moved scripts into the scripts folder

---
+++ Unity Project Files/Assets/Standard Assets/Scripts/WeaponPickup.js
@@ -0,0 +1,112 @@
+#pragma strict
+
+static var selectedWeapon : String;
+
+//variables for weapons
+private var medievalBlade : GameObject;
+private var weapon2       : GameObject;
+private var weapon3       : GameObject;
+
+//once equipped, can't un-equip- MIGHT CHANGE THIS
+static var leftEquipped :boolean = false;
+static var rightEquipped:boolean = false;
+
+var weapon = ["MedievalBlade","Weapon2","Weapon3"];
+private var weaponNum : int;//stores array value
+
+function Start () {
+	medievalBlade = GameObject.FindGameObjectWithTag(weapon[0]);
+	weapon2       = GameObject.FindGameObjectWithTag(weapon[1]);
+	//weapon3       = GameObject.FindGameObjectWithTag(weapon[2]);
+}
+
+function Update () {
+
+	if(selectedWeapon == weapon[0])
+	{	
+		if(Input.GetKeyDown(KeyCode.Alpha1)) && !leftEquipped)
+		{
+			drawWeapons(medievalBlade, true);
+		}
+		else if(Input.GetKeyDown(KeyCode.Alpha5)) && !rightEquipped)
+		{
+			drawWeapons(medievalBlade, false);
+		}
+	}
+	else if(selectedWeapon == weapon[1])
+	{	
+		if(Input.GetKeyDown(KeyCode.Alpha1)) && !leftEquipped)
+		{
+			drawWeapons(weapon2, true);
+		}
+		else if(Input.GetKeyDown(KeyCode.Alpha5)) && !rightEquipped)
+		{
+			drawWeapons(weapon2, false);
+		}
+	}
+	else if(selectedWeapon == weapon[2])
+	{
+		if(Input.GetKeyDown(KeyCode.Alpha1)) && !leftEquipped)
+		{
+			drawWeapons(weapon3, true);
+		}
+		else if(Input.GetKeyDown(KeyCode.Alpha5)) && !rightEquipped)
+		{
+			drawWeapons(weapon3, false);
+		}
+	}
+}
+
+//left is true for left hand, false for right
+function drawWeapons(chosenWeapon:GameObject, left:boolean)
+{
+	Destroy(chosenWeapon);
+	
+	if(left)
+	{
+		PlayerHUD.toDrawLeft = selectedWeapon;
+		leftEquipped = true;
+	}
+	else//righthand equip
+	{
+		PlayerHUD.toDrawRight = selectedWeapon;
+		rightEquipped = true;
+	}
+			
+	selectedWeapon = "";
+}
+
+function OnTriggerEnter(col:Collider)
+{
+	if(col.gameObject.tag == "Player")
+	{
+		selectedWeapon = this.tag;
+		
+		if(selectedWeapon == weapon[0])
+			weaponNum = 0;
+		else if(selectedWeapon == weapon[1])
+			weaponNum = 1;
+		else//(selectedWeapon == weapon[2])
+			weaponNum = 2;
+	}
+}
+
+function OnTriggerExit(other:Collider){
+	if(other.gameObject.tag=="Player")
+		selectedWeapon ="";
+}
+
+function OnGUI(){
+	if(selectedWeapon != "")
+	{
+		printEquip(weaponNum);
+	}
+	//else{/*no weapon selected*/}
+
+}
+
+function printEquip(selected : int)
+{
+	GUI.Label(Rect((Screen.width/2)-50,Screen.height-500,(Screen.width/2)-250,Screen.height-175),
+	"Equip " + weapon[selected] +"?\n\nPress 1 for LeftHand Equip\nPress 5 for RightHand Equip");
+}
